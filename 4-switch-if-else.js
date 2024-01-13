de8a6a1da3d86a75afdec3ec8eebe8b77ffdd8f0
tkozlovets <tkozlovets@ukr.net>
2024-01-13 13:53:56 +0100

    Add files via upload

---
+++ 4-switch-if-else.js
@@ -0,0 +1,49 @@
+
+// if - else
+if(4==4){
+    console.log('Expression is true')
+}
+
+if(4==4){
+    console.log('Expression is true')
+}else{
+    console.log('Expression is false')
+}
+
+
+let num = 2;
+
+if(num === 1){
+    console.log('Expression is 1')
+} else if (num === 2){
+    console.log('Expression is 2')
+} else if (num === 3){
+    console.log('Expression is 3')
+} else if (num === 4){
+    console.log('Expression is 4')
+} else {
+    console.log('Expression is not 1, 2, 3 or 4')
+}
+
+// тернарний оператор / if-else
+(num === 50) ? console.log('Expression is true') : console.log('Expression is false')
+
+
+// switch-case
+
+let number = 50;
+
+switch(number){
+    case 49:
+        console.log('Value = 49');
+        break;
+    case 50:
+        console.log('Value = 50');
+        break;
+    case 51:
+        console.log('Value = 51');
+        break;
+    default:
+        console.log('Value = 51');
+        break;
+}
